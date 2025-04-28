17559481b87195a129fdeaa6ec75ef5469e99134
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2025-04-28 17:42:48 +0200

    Create Lab1_1.ino

---
+++ ELK/Lab1_1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Myroslav Natalchenko");
+  delay(2000);
+  Serial.println("Kiryl Sankouski");
+  delay(2000);
+}
