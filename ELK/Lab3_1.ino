076b20e678ca6950005e4de5762771a14273bd69
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2025-05-07 09:20:18 +0200

    Update Lab3_1.ino

--- ELK/Lab3_1.ino
+++ ELK/Lab3_1.ino
@@ -1,1 +1,35 @@
-dd
+byte digits[10] = {
+  0b00111111, 
+  0b00000110, 
+  0b01011011, 
+  0b01001111, 
+  0b01100110, 
+  0b01101101, 
+  0b01111101, 
+  0b00000111, 
+  0b01111111, 
+  0b01101111
+};
+
+int pins[8]={
+  2,3,4,5,6,7,8,9
+};
+
+void displayDigit(byte digitByte) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(pins[i], !bitRead(digitByte, i));
+  }
+}
+
+void setup() {
+  for (int i=0;i<=7;i++){
+    pinMode(pins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i <= 9; i++) {
+    displayDigit(digits[i]);
+    delay(1000); 
+  }
+}
